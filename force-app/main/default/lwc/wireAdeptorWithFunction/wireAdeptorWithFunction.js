66bbf3e80873a6665a736b9bb7c2f4ffbe60d001
Rohit Kumar <rkcpratap@gmail.com>
2025-06-26 22:08:49 +0530

    UpdateComment

--- force-app/main/default/lwc/wireAdeptorWithFunction/wireAdeptorWithFunction.js
+++ force-app/main/default/lwc/wireAdeptorWithFunction/wireAdeptorWithFunction.js
@@ -11,6 +11,11 @@ export default class WireAdeptorWithFunction extends LightningElement {
   accounts;
   errors;
   columns = columns;
+  //Wire Adeptor with Function
+  //This is a wire adapter with function that will be called when the data is available
+  //It will update the accounts data by adding a default value for Rating if it is not present
+  //This is useful when you want to modify the data before it is used in the component
+  //In this case, we are adding a default value of "Warm" for Rating
   @wire(getAccounts) getAccFunction({ data, error }) {
     if (data) {
       let updateAccounts = data.map((curritem) => {
